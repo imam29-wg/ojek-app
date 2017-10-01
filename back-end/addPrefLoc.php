c91eb5197b73da4fa01036b8ec368cd74e8531ae
Arya Pradipta <aryapradipta9@gmail.com>
2017-10-01 16:50:22 +0700

    add edit preference location page

---
+++ back-end/addPrefLoc.php
@@ -0,0 +1,15 @@
+<?php
+		
+        function connectTOSQL(){
+        	return mysqli_connect("localhost", "root", "", "ojek");
+    	}
+        $id = $_POST['id'];
+        $loc = $_POST['loc'];
+        $db = connectTOSQL();
+        $usernamesql = "INSERT INTO pref_location VALUES ('$id','$loc')";
+        $username_result = mysqli_query($db, $usernamesql);
+        header("Location: ../front-end/editPrefLoc.php?id_active=$id");
+        
+        //echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../front-end/editPrefLoc.php?id_active=$id">';
+        exit();
+?>
