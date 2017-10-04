ef9946e0f91db0ba146462ef69e1e938edaddaf1
pram <rizki.ihza@gmail.com>
2017-10-04 19:39:45 +0700

    rubah profil.php menjadi mengirimkan json string

--- back-end/profil.php
+++ back-end/profil.php
@@ -10,6 +10,7 @@
             return mysqli_connect("localhost", "root", "", "ojek");
         }   
         $id = $_SESSION['login_user'];
+        // $id=1;
         $db = connectTOSQL();
         $usersql = "select * from profil where ID = '$id'";
         $user_result = mysqli_query($db, $usersql);
@@ -17,7 +18,9 @@
         $username = $user_row['Username'];
         $driver = $user_row['Driver'];
 
-        echo $user_row['Name'] . " " . $user_row['Username'] . " " . $user_row['Email'] . " " . $user_row['Phone'] . " " . $user_row['Driver'] . " " . $user_row['foto'] . " ";
+        $final_object = array("Name" => $user_row['Name'], "Username" => $user_row['Username'], "Email" => $user_row['Email'], "Phone" => $user_row['Phone'], "Driver" => $user_row['Driver'], "Foto" => $user_row['foto']);
+
+        // echo $user_row['Name'] . " " . $user_row['Username'] . " " . $user_row['Email'] . " " . $user_row['Phone'] . " " . $user_row['Driver'] . " " . $user_row['foto'] . " ";
 
         //menghitung rating
         if($driver[0] == 1) {
@@ -32,19 +35,18 @@
             $driver_location_row = mysqli_fetch_array($driver_result, MYSQLI_ASSOC);
             $driver_location = $driver_location_row['Location'];   
             
-            echo $driver_avg_rating;
-            echo " ";
-            echo $driver_vote;
-            echo " ";
-            echo sizeof($driver_location);
-            echo  " ";
-            foreach (array($driver_location) as $value) {
-                echo $value;
-                echo " ";
+            $final_object['avg_rating'] = $driver_avg_rating;
+            $final_object['vote'] = $driver_vote;
+            
+            $location = array();
+            foreach(array($driver_location) as $value) {
+                array_push($location, $value);
             }
+
         }
 
-
+        $final_json = json_encode($final_object,JSON_UNESCAPED_SLASHES);
+        echo $final_json;
 
         connectTOSQL();
 
