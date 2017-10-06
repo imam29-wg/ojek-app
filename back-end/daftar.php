00cb5a1d9d058c12d36bce5f608363423925e23b
rizkihalasan <rizki_halasan@yahoo.com>
2017-10-06 16:38:55 +0700

    validasi register

--- back-end/daftar.php
+++ back-end/daftar.php
@@ -4,31 +4,14 @@
         return mysqli_connect("localhost", "root", "", "ojek");
     }
 
-    // Fungsi untuk memvalidasi form pendaftaran
-    function validasiDaftar($name, $username, $email, $password, $confirm, $phone, $driver){
-        if(strlen($password) < 8){
-            return false;
-        }
-        if ($password != $confirm){
-            return false;
-        }
-        return true;
-    }
-
-
     function masukkanData($db, $name, $username, $email, $password, $confirm, $phone, $driver){
-        if (validasiDaftar($name, $username, $email, $password, $confirm, $phone, $driver)){
-            $sql = "insert into profil(Name, Username, Email, Password, Phone, Driver) values('$name', '$username', '$email', '$password', '$phone', '$driver')";
-            if (mysqli_query($db, $sql)){
-                echo "berhasil";
-            }
-            else{
-                echo "gagal";
-            }
-        }
-        else{
-            echo "Data tidak valid";
-        }
+         $sql = "insert into profil(Name, Username, Email, Password, Phone, Driver) values('$name', '$username', '$email', '$password', '$phone', '$driver')";
+         if (mysqli_query($db, $sql)){
+            echo "berhasil";
+         }
+         else{
+            echo "gagal";
+         }
     }
     session_start();
     $db = connectTOSQL();
@@ -40,12 +23,13 @@
     $confirm = $_POST['confirm'];
     $phone = $_POST['phone'];
     $driver = $_POST['driver'];
+    if ($driver){
+        $driver = 1;
+    }
+    else{
+        $driver = 0;
+    }
 
-    /*$sql = "select * from profil";
-    $result = mysqli_query($db, $sql);
-    $count = mysqli_num_rows($result);
-    $count++;
-    */
     masukkanData($db, $name, $username, $email, $password, $confirm, $phone, $driver);
 
     header("Location: ../front-end/login.html?");
